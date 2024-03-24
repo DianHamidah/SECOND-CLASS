4d5385628f78c9f806e6bb3a3e9cace3148d4f8b
dian nurdiani <diannurdianih@gmail.com>
2024-03-24 10:29:45 +0700

    SECOND CLASS

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(hasEqualSum(1, 1, 1)); // false
+        System.out.println(hasEqualSum(1, 1, 2)); // true
+        System.out.println(hasEqualSum(1, -1, 0)); // true
+    }
+
+    public static boolean hasEqualSum(int num1, int num2, int sum) {
+        return (num1 + num2) == sum;
+    }
+}
+
+
